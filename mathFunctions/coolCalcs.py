04c8d96e96ea18192d9fc9e88ee72194ca727caa
franciscouranga52 <francisco.uranga52@journalism.cuny.edu>
2023-10-30 14:33:05 -0400

    Upload

---
+++ mathFunctions/coolCalcs.py
@@ -0,0 +1,15 @@
+##function to add two numbers
+def addNumbers (number1, number2):
+    '''
+    Provide 2 numbers as parameters.
+    Fuction returns total
+    '''
+    return number1 + number2
+
+##function to substract two numbers
+def substractNumbers (number1, number2):
+    '''
+    Provide 2 numbers as parameters.
+    Fuction returns number 1 - number 2
+    '''
+    return number1 - number2
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pip-package-demo', ##name of repo enclosing folder (pip-package-demo)
+    version='0.0.1',
+    author='Francisco Uranga', ## your name
+    author_email='francisco.c.uranga@gmail.com', ## your email
+    description='Demo on pip package creation', ## description of package
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/franciscouranga52/testFunc',# url to repo
+    project_urls = {
+        "Bug Tracker": "https://github.com/franciscouranga52/testFunc/issues"
+    }, ## url to issues page on your repo
+    license='MIT',
+    packages=['mathFunctions'], ## name of folder that holds the functions
+    install_requires=[], ## names of packages required to run your functions
+)
